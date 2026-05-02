891c0bb9207f8c2c3d7d0a4db078dcec7baa7b46
diegorezm <dmourarezende@gmail.com>
2026-05-02 16:45:54 -0300

    feat: user type schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,15 @@
 import { env } from "cloudflare:workers";
+import type { ExtractTablesWithRelations } from "drizzle-orm";
 import { type DrizzleD1Database, drizzle } from "drizzle-orm/d1";
+import type { SQLiteTransaction } from "drizzle-orm/sqlite-core";
 import * as schema from "./schema.ts";
 
 export const db = drizzle(env.DB, { schema });
-export type DB = DrizzleD1Database<typeof schema>;
+export type DB =
+	| DrizzleD1Database<typeof schema>
+	| SQLiteTransaction<
+		"async",
+		D1Result<unknown>,
+		typeof schema,
+		ExtractTablesWithRelations<typeof schema>
+	>;
