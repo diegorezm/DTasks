95940aedfdf3bd0cd2b83b08bfa95c11c4ea9959
diegorezm <dmourarezende@gmail.com>
2026-05-13 17:51:26 -0300

    added private routes with auth middleware

--- src/routes/_private.tsx
+++ src/routes/_private.tsx
@@ -1,16 +1,16 @@
-import { getCurrentSession } from "@/features/auth/actions";
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
+import { getCurrentSession } from "@/features/auth/actions";
 
 export const Route = createFileRoute("/_private")({
-	beforeLoad: async ({ location }) => {
-		const { data, error } = await getCurrentSession();
-		if (error || !data) {
-			throw redirect({
-				to: "/sign-in",
-				search: { redirected: location },
-			});
-		}
-		return { session: data };
-	},
-	component: () => <Outlet />,
+  beforeLoad: async ({ location }) => {
+    const { data, error } = await getCurrentSession();
+    if (error || !data) {
+      throw redirect({
+        to: "/sign-in",
+        search: { redirected: location },
+      });
+    }
+    return { session: data };
+  },
+  component: () => <Outlet />,
 });
