e46243ef799f52ce8ff8e8a85793162fbe03b782
diegorezm <dmourarezende@gmail.com>
2026-05-13 17:51:49 -0300

    now each schema has its own file

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,7 @@
 import { env } from "cloudflare:workers";
 import { drizzle } from "drizzle-orm/d1";
+import type { BaseSQLiteDatabase } from "drizzle-orm/sqlite-core";
 import { relations, schema } from "./schemas";
 
 export const db = drizzle(env.DB, { schema, relations });
-
-export type DB =
-	| typeof db
-	| Parameters<Parameters<typeof db.transaction>[0]>[0];
+export type DB = BaseSQLiteDatabase<"async" | "sync", any, typeof schema>;
